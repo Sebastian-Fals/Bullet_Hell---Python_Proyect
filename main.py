6f8fc07c0f2e65abe5a965e98a5977642bbbfe2c
Sebastian Fals <77508405+Sebastian-Fals@users.noreply.github.com>
2024-03-09 15:31:52 -0500

    nada importante

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pygame import *
 from math import *
+import pygame_shaders
 
 #Se importan las clases y demás
 from GameEntities import Player, Enemies
@@ -9,9 +10,14 @@ init()
 
 #Configuración de la pantalla
 resolucion = (1920, 1080)
-screen = display.set_mode(resolucion, FULLSCREEN)
-clock = time.Clock()
+
+
+screen = display.set_mode(resolucion, OPENGL | FULLSCREEN)
 screen_size = screen.get_size()
+display = Surface(screen_size)
+screen_shader = pygame_shaders.Shader(screen_size, screen_size, (0, 0), )
+
+clock = time.Clock()
 #----------------------------
 
 #Colores
@@ -19,19 +25,16 @@ WHITE = (255, 255, 255, 255)
 BLACK = (0, 0, 0, 0)
 DARK_BLUE = (0, 1, 35, 14)
 #----------------------------
-#Grupos
-bullets = sprite.Group()
-enemies = sprite.Group()
-#----------------------------
+
 #Variables
 player_sprite = "Assets/tiny_ship14.png"
 bullet_sprites = "Assets/laser_beam.png"
 #----------------------------
 
 #Objetos
+All_sprite_in_game = sprite.Group()
 player = Player(player_sprite, bullet_sprites, 500, 500, 40, 40, 10)
 enemie = Enemies("Assets/circular_enemy.png", bullet_sprites, 300, 300, 40, 40, 10, "enemigo_patron_circular")
-enemies.add(enemie)
 #----------------------------
 
 #Logica de los niveles o pantallas
@@ -43,7 +46,7 @@ while running:
         if e.type == QUIT:
             running = False
         #Para que funcione el disparo
-        player.Shoot(e, bullets)
+        player.Shoot(e, All_sprite_in_game)
 
     screen.fill(DARK_BLUE)
 
@@ -51,10 +54,12 @@ while running:
 
     screen_rect = display.get_surface().get_rect()
 
-        #Posicion del mouse
+    #Posicion del mouse
     mouse_pos = mouse.get_pos()
 
+<<<<<<< HEAD
         #Logica de las colisiones
+        #Colisiones de los enemigos con las balas
     bullets_to_enemies = sprite.groupcollide(enemies, bullets, False, False)
 
     for enemy, bullet in bullets_to_enemies.items():
@@ -64,18 +69,20 @@ while running:
                 b.kill()
 
         #Update de los objetos de la escena
+=======
+>>>>>>> parent of 132c137 (Se agrega mecánica de matar a los enemigos y se optimiza el disparo)
     player.update(player_sprite, mouse_pos, screen_size)
-    enemies.update(bullets)
-    bullets.update(screen_rect)
+    enemie.update(All_sprite_in_game)
+    screen.blit(player.image, player.rect)
+    screen.blit(enemie.image, enemie.rect)
+    All_sprite_in_game.update(screen_rect)
     #-----------------
 
     #Configuración de la pantalla (In-Game)
-    screen.blit(player.image, player.rect) #Se dibuja el player en pantalla
-    bullets.draw(screen)
-    enemies.draw(screen)
+    All_sprite_in_game.draw(screen)
     display.flip()
-
-    clock.tick(60)#Control de los fps o cuadros por segundo
+    #Control de los fps o cuadros por segundo
+    clock.tick(60)
     #-----------------
 
 quit()
